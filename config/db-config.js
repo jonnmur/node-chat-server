bf03ad823d923c704b5de6a048a8cf32d03d5403
Joonas Murand <joonasmurand@gmail.com>
2023-03-11 20:29:33 +0200

    Add timezone

--- config/db-config.js
+++ config/db-config.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: 'mariadb'
+    dialect: 'mariadb',
+    timezone: '+02:00',
 });
 
 module.exports = sequelize;
